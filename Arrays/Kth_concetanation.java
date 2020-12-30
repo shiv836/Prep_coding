92afa315d73b0098128a776c66315a8891c39f75
Shivansh <tshivansh2499@gmail.com>
2020-12-30 23:40:58 +0530

    added comments

--- Arrays/Kth_concetanation.java
+++ Arrays/Kth_concetanation.java
@@ -10,6 +10,15 @@ class Codechef {
 public static void main(String[] args){
 Scanner sc=new Scanner(System.in);
 int t=sc.nextInt();
+// kadane algorithm.
+// -evaluate current sum.
+// -update the current sum.
+//-set the current sum to zero if the current sum reaches to less than zero.
+//return the anser at the end.
+//in kth concatenaation problem, if k=1 ,then simply apply kadane algorithm .
+// if k>1, then if the current sum is greater than zero, then the subset sum would be kadane algorithm on two  + (k-2)*currSum;
+// if the currentSum is less than zero than the subset sum would be kadane on k=2;
+
 while(t>0)
 {
 int n=sc.nextInt();
